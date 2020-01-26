77889552a8e82aaa1620eb6c439567fb2fec4404
Dan Bennett <dghbennett47@gmail.com>
2020-01-26 16:17:15 +0000

    feat: draw level

--- episode-1/public/js/main.js
+++ episode-1/public/js/main.js
@@ -1,14 +1,5 @@
 import SpriteSheet from './SpriteSheet.js';
-
-function loadImage(url) {
-    return new Promise(resolve =>{
-        const image = new Image();
-        image.addEventListener('load', () =>{
-            resolve(image)
-        });
-        image.src = url;
-    });
-}
+import {loadImage} from './loaders.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
@@ -19,5 +10,17 @@ loadImage('/img/tiles.png')
 .then(image => {
     const sprites = new SpriteSheet(image, 16, 16);
     sprites.define('ground', 0, 0);
-    sprites.draw('ground', context, 45, 62)
+    sprites.define('sky', 3, 23);
+
+    for(let x = 0; x < 25; x++){
+        for(let y = 0; y < 14; y++){
+            sprites.drawTile('sky', context, x, y)
+        }
+    }
+
+    for(let x = 0; x < 25; x++){
+        for(let y = 12; y < 14; y++){
+            sprites.drawTile('ground', context, x, y)
+        }
+    }
 })
