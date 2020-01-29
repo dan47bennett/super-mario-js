b6fb8fc30637e5cdb1e0911837e11e2493d02de7
Dan Bennett <dghbennett47@gmail.com>
2020-01-29 23:05:16 +0000

    feat: add mario and introduce movement

--- episode-2/public/js/main.js
+++ episode-2/public/js/main.js
@@ -1,5 +1,5 @@
-import SpriteSheet from './SpriteSheet.js';
-import {loadImage, loadLevel} from './loaders.js';
+import {loadLevel} from './loaders.js';
+import {loadBackgroundSprites, loadMarioSprite} from './sprites.js';
 
 function drawBackground(background, context, sprites) {
     background.ranges.forEach(([x1, x2, y1, y2]) => {
@@ -11,26 +11,33 @@ function drawBackground(background, context, sprites) {
     });
 }
 
-
-function loadBackgroundSprites() {
-    return loadImage('/img/tiles.png')
-    .then(image => {
-        const sprites = new SpriteSheet(image, 16, 16);
-        sprites.define('ground', 0, 0);
-        sprites.define('sky', 3, 23);
-        return sprites;
-    });
-}
-
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 Promise.all([
+    loadMarioSprite(),
     loadBackgroundSprites(),
     loadLevel('1-1')
 ])
-.then(([sprites, level]) => {
+.then(([marioSprite, sprites, level]) => {
+    const backgroundBuffer = document.createElement('canvas')
+    backgroundBuffer.width = 256;
+    backgroundBuffer.height = 240
     level.backgrounds.forEach(background => {
-    drawBackground(background, context, sprites);
+        drawBackground(background, backgroundBuffer.getContext('2d'), sprites);
     });
+
+    const position = {
+        x: 64,
+        y: 64
+    }
+
+    function update() {
+        context.drawImage(backgroundBuffer, 0, 0)
+        marioSprite.draw('idle', context, position.x, position.y)
+        position.x += 2;
+        position.y += 2;
+        requestAnimationFrame(update)
+    }
+    update()
 });
