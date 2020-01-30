9f67e1445523aa064a3f1534de4145ad379c1a89
Dan Bennett <dghbennett47@gmail.com>
2020-01-30 11:11:54 +0000

    feat: abstract Compositor and layers

--- episode-2/public/js/main.js
+++ episode-2/public/js/main.js
@@ -1,18 +1,18 @@
 import {loadLevel} from './loaders.js';
 import {loadBackgroundSprites, loadMarioSprite} from './sprites.js';
-
-function drawBackground(background, context, sprites) {
-    background.ranges.forEach(([x1, x2, y1, y2]) => {
-        for(let x = x1; x < x2; ++x){
-            for(let y = y1; y < y2; ++y){
-                sprites.drawTile(background.tile, context, x, y)
-            }
-        }
-    });
-}
+import Compositor from './Compositor.js'
+import {createBackgroundLayer} from './layers.js'
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
+
+
+function createSpriteLayer(sprite, position) {
+    return function drawSpriteLayer(context) {
+        sprite.draw('idle', context, position.x, position.y);
+    }
+}
+
 
 Promise.all([
     loadMarioSprite(),
@@ -20,21 +20,22 @@ Promise.all([
     loadLevel('1-1')
 ])
 .then(([marioSprite, sprites, level]) => {
-    const backgroundBuffer = document.createElement('canvas')
-    backgroundBuffer.width = 256;
-    backgroundBuffer.height = 240
-    level.backgrounds.forEach(background => {
-        drawBackground(background, backgroundBuffer.getContext('2d'), sprites);
-    });
+    const comp = new Compositor();
+
+
+    const backgroundLayer = createBackgroundLayer(level.backgrounds, sprites);
+    comp.layers.push(backgroundLayer);
 
     const position = {
         x: 64,
         y: 64
     }
 
+    const spriteLayer = createSpriteLayer(marioSprite, position);
+    comp.layers.push(spriteLayer)
+
     function update() {
-        context.drawImage(backgroundBuffer, 0, 0)
-        marioSprite.draw('idle', context, position.x, position.y)
+        comp.draw(context);
         position.x += 2;
         position.y += 2;
         requestAnimationFrame(update)
